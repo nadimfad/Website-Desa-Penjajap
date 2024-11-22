a0f76f4ee0295e9026942dca712a8be39b538214
MohdNadimFadillah <d1041211074@student.untan.ac.id>
2024-11-23 00:35:40 +0700

    commit ketiga

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -180,6 +180,26 @@
     </div>
 </div>
 
+<div class="map-container">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12153.866873453797!2d108.97384167364818!3d1.187490384077525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31e48325dc3b5689%3A0x9d98909ee32ca45c!2sPenjajap%2C%20Kec.%20Pemangkat%2C%20Kabupaten%20Sambas%2C%20Kalimantan%20Barat!5e1!3m2!1sid!2sid!4v1732296728087!5m2!1sid!2sid" 
+    width="350" 
+    height="350" 
+    style="border:0;" 
+    allowfullscreen="" 
+    loading="lazy" 
+    referrerpolicy="no-referrer-when-downgrade">
+</iframe>
+</div>
+
+<style>
+.map-container iframe {
+    width: 100%;
+    height: 300px;
+    border: none;
+}
+
+</style>
+
 <!-- jQuery, Popper.js, Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
