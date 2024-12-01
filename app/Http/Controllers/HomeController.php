448e864140d342adfd88fdf7e9120be900b6adf8
Vynnov <kevinnov071103@gmail.com>
2024-12-02 02:47:29 +0700

    lempar sambutan, employee, dan galery

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Banner;
+use App\Models\Sambutan;
+use App\Models\Employee;
+use App\Models\Gallery;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -10,7 +13,10 @@ class HomeController extends Controller
     public function index()
     {
         return view('home', [
-            "banners" => Banner::all()
+            "banners" => Banner::all(),
+            "sambutans"=> Sambutan::first(),
+            "employees" => Employee::all(),
+            "galleries" => Gallery::all()->take(6),
         ]);  // Ensure 'home.blade.php' exists in resources/views
     }
 }
