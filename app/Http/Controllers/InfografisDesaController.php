e8979fe0d19f3667bebcd1f577e4bdf0c391c343
Vynnov <kevinnov071103@gmail.com>
2024-12-02 04:22:43 +0700

    lempar database agar masuk ke views user

--- app/Http/Controllers/InfografisDesaController.php
+++ app/Http/Controllers/InfografisDesaController.php
@@ -1,13 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Infographic;
+use App\Models\Job;
 use Illuminate\Http\Request;
 
 class InfografisDesaController extends Controller
 {
     public function index()
     {
-     return view('infografis');
+     return view('infografis',[
+        "infografis" => Infographic::first(),
+        "jobs" => Job::all(),
+     ]);
     }
 }
